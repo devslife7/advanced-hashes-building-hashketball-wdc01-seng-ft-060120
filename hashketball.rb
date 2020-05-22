3ec7d0995a0e37739f97b87f8289fb3351bb66d5
Marcos Velasco <devslife7@gmail.com>
2020-05-22 02:15:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -102,7 +102,7 @@ def game_hash
             :slam_dunks => 0
           },
           {
-            :player_name => "",
+            :player_name => "Ben Gordon",
             :number => 0,
             :shoe => 0,
             :points => 0,
