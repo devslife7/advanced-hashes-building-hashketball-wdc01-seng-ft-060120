a002911f9677aa5f2313ded69dcf88a9ef1e5565
Marcos Velasco <devslife7@gmail.com>
2020-05-22 02:15:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -91,7 +91,7 @@ def game_hash
             :slam_dunks => 0
           },
           {
-            :player_name => "",
+            :player_name => "DeSagna Diop",
             :number => 0,
             :shoe => 0,
             :points => 0,
