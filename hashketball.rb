73157b9baedd8630729484fddb586ec4e79f198b
Marcos Velasco <devslife7@gmail.com>
2020-05-22 02:14:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -52,7 +52,7 @@ def game_hash
             :slam_dunks => 0
           },
           {
-            :player_name => "",
+            :player_name => "Jason Terry",
             :number => 0,
             :shoe => 0,
             :points => 0,
