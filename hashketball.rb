b96e978965f5ec9638b8d386ae4b5af618f94e07
Marcos Velasco <devslife7@gmail.com>
2020-05-22 02:14:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,7 +30,7 @@ def game_hash
             :slam_dunks => 0
           },
           {
-            :player_name => "",
+            :player_name => "Brook Lopez",
             :number => 0,
             :shoe => 0,
             :points => 0,
